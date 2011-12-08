7899dae5f57f87061b616aadac717e834b8c7761
Davide Rizzi <sadjehwty@gmail.com>
2011-12-08 12:28:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baka::Application.config.secret_token = '3304468336633dd3eb7be9ff9d47a6c4c46e5e96388a5dcfba40a4884d3b00c513068849b53dc1dedf8c0f59879ba78f707c32a1266199a6be22a0209c157507'
