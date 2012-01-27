e589108bee0a6778de49641098c2dfbc08d538cd
Davide Rizzi <sadjehwty@gmail.com>
2012-01-27 19:33:50 +0100

    conflitto tra devise e un modello precedente

--- db/migrate/20120124174823_create_users.rb
+++ db/migrate/20120124174823_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email
-      t.string :password
       t.date :birthday
       t.string :sign
       t.string :avatar,:default=>"/images/users/guest.png"
