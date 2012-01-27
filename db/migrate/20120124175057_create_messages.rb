813be5b1e59bad60a4322e470f316ebcebe7b712
Davide Rizzi <sadjehwty@gmail.com>
2012-01-27 19:35:51 +0100

    non null in messages

--- db/migrate/20120124175057_create_messages.rb
+++ db/migrate/20120124175057_create_messages.rb
@@ -1,14 +1,14 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.string :text
-      t.bool :section,:default=>false
-      t.bool :pinned,:default=>false
-      t.int :lft
-      t.int :rgt
-      t.string :title
-      t.int :message_id
-      t.int :user_id
+      t.string :text,:null=>false
+      t.boolean :section,:default=>false
+      t.boolean :pinned,:default=>false
+      t.integer :lft,:null=>false
+      t.integer :rgt,:null=>false
+      t.string :title,:null=>false
+      t.integer :message_id,:null=>false
+      t.integer :user_id,:null=>false
 
       t.timestamps
     end
