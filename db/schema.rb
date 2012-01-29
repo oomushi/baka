6c83cd6836196551e4bd2c9d18f2a9ec84598dd3
Davide Rizzi <sadjehwty@gmail.com>
2012-01-29 13:13:46 +0100

    retrodatato una migrazione

--- db/schema.rb
+++ db/schema.rb
@@ -11,46 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120124175057) do
+ActiveRecord::Schema.define(:version => 20120124174912) do
 
-  create_table "messages", :force => true do |t|
-    t.string   "text",                          :null => false
-    t.boolean  "section",    :default => false
-    t.boolean  "pinned",     :default => false
-    t.integer  "lft",                           :null => false
-    t.integer  "rgt",                           :null => false
-    t.string   "title",                         :null => false
-    t.integer  "message_id",                    :null => false
-    t.integer  "user_id",                       :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "users", :id => false, :force => true do |t|
+    t.integer "id"
   end
-
-  create_table "users", :force => true do |t|
-    t.string   "username"
-    t.date     "birthday"
-    t.string   "sign"
-    t.string   "avatar",                                :default => "/images/users/guest.png"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "email",                                 :default => "",                        :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "",                        :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-  end
-
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
