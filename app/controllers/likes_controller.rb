3a6754dcdffeba15f7b79bdb12f0d6787f60d819
Davide Rizzi <sadjehwty@gmail.com>
2012-02-12 14:01:33 +0100

    ridote azioni per like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,55 +1,13 @@
 class LikesController < ApplicationController
-  # GET /likes
-  # GET /likes.json
-  def index
-    @likes = Like.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @likes }
-    end
-  end
-
-  # GET /likes/1
-  # GET /likes/1.json
-  def show
-    @like = Like.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @like }
-    end
-  end
-
-  # GET /likes/new
-  # GET /likes/new.json
-  def new
-    @like = Like.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @like }
-    end
-  end
-
-  # GET /likes/1/edit
-  def edit
-    @like = Like.find(params[:id])
-  end
 
   # POST /likes
   # POST /likes.json
   def create
     @like = Like.new(params[:like])
-
-    respond_to do |format|
-      if @like.save
-        format.html { redirect_to @like, notice: 'Like was successfully created.' }
-        format.json { render json: @like, status: :created, location: @like }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
+    if @like.save
+      redirect_to @like.message, notice: 'Like was successfully created.'
+    else
+      redirect_to @like.message
     end
   end
 
@@ -57,15 +15,10 @@ class LikesController < ApplicationController
   # PUT /likes/1.json
   def update
     @like = Like.find(params[:id])
-
-    respond_to do |format|
-      if @like.update_attributes(params[:like])
-        format.html { redirect_to @like, notice: 'Like was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
+    if @like.update_attributes(params[:like])
+      redirect_to @like.message, notice: 'Like was successfully updated.'
+    else
+      redirect_to @like.message
     end
   end
 
@@ -73,11 +26,8 @@ class LikesController < ApplicationController
   # DELETE /likes/1.json
   def destroy
     @like = Like.find(params[:id])
+    message=@like.message
     @like.destroy
-
-    respond_to do |format|
-      format.html { redirect_to likes_url }
-      format.json { head :ok }
-    end
+    redirect_to @like.message
   end
 end
