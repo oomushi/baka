9e4b0eaba191e282ca13b950be541531c86b49aa
Davide Rizzi <sadjehwty@gmail.com>
2012-02-17 20:57:00 +0100

    terminato modello avatar

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -2,7 +2,23 @@ class Avatar < ActiveRecord::Base
   validates_presence_of :user_id
   validates_uniqueness_of :user_id
   belongs_to :user
+  validate :right_size?
+  
   def to_s
-    self.url.nil? ? "/avatars/#{self.id}"  : self.url
+    self.url=='' ? "/avatars/#{self.id}"  : self.url
+  end
+   
+  def uploaded_data= image
+    self.file = image.read
+#   self.content_type = image.content_type.chomp TODO da aggiungere il campo con una migration
+  end
+    
+  protected
+  def right_size?
+#   unless self.file.nil?
+    image = MiniMagick::Image.read self.file
+    errors.add(:uploaded_data, "width to high") if image['width'] > 128 # parametro da prendere da qualche altra parte
+    errors.add(:uploaded_data, "hight to high") if image['height'] > 128 # parametro da prendere da qualche altra parte
+#   end
   end
 end
