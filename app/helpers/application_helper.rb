665ad426bea128d7d934fe8c4c287f0c3acdaf3d
Davide Rizzi <sadjehwty@gmail.com>
2012-02-18 10:10:52 +0100

    bb non da errore se la stringa è nulla

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def bb text
-    bb = RbbCode::Parser.new
-    bb.parse(text).html_safe
+    if ! text.nil? and text!=''
+      bb = RbbCode::Parser.new
+      bb.parse(text).html_safe
+    end
   end
 end
