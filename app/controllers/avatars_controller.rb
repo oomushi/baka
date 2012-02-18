d539c86f8ade59a4091145bdcbb75d1de171efe0
Davide Rizzi <sadjehwty@gmail.com>
2012-02-18 10:09:31 +0100

    nuovi tipi di render per le immagini

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,56 +1,26 @@
 class AvatarsController < ApplicationController
-  # GET /avatars
-  # GET /avatars.json
-  def index
-    @avatars = Avatar.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @avatars }
-    end
-  end
-
   # GET /avatars/1
   # GET /avatars/1.json
   def show
+
     @avatar = Avatar.find(params[:id])
-
+    
     respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @avatar }
-    end
-  end
-
-  # GET /avatars/new
-  # GET /avatars/new.json
-  def new
-    @avatar = Avatar.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @avatar }
+      format.jpg  { send_data(@avatar.file, 
+                              :type  => 'image/jpeg', 
+                              :disposition => 'inline') }
+      format.gif  { send_data(@avatar.file, 
+                              :type  => 'image/gif', 
+                              :disposition => 'inline') }
+      format.png  { send_data(@avatar.file, 
+                              :type  => 'image/png', 
+                              :disposition => 'inline') }
     end
   end
 
   # GET /avatars/1/edit
   def edit
     @avatar = Avatar.find(params[:id])
-  end
-
-  # POST /avatars
-  # POST /avatars.json
-  def create
-    @avatar = Avatar.new(params[:avatar])
-
-    respond_to do |format|
-      if @avatar.save
-        format.html { redirect_to @avatar, notice: 'Avatar was successfully created.' }
-        format.json { render json: @avatar, status: :created, location: @avatar }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @avatar.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PUT /avatars/1
@@ -60,10 +30,10 @@ class AvatarsController < ApplicationController
 
     respond_to do |format|
       if @avatar.update_attributes(params[:avatar])
-        format.html { redirect_to @avatar, notice: 'Avatar was successfully updated.' }
+        format.html { redirect_to @avatar.user,:action=>'edit', notice: 'Avatar was successfully updated.' }
         format.json { head :ok }
       else
-        format.html { render action: "edit" }
+        format.html { redirect_to @avatar.user,:action=>'edit' }
         format.json { render json: @avatar.errors, status: :unprocessable_entity }
       end
     end
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,7 @@
 require File.expand_path('../application', __FILE__)
 #require 'mini_magick'
 # Initialize the rails application
+Mime::Type.register 'image/jpeg', :jpg
+Mime::Type.register 'image/gif',  :gif
+Mime::Type.register 'image/png',  :png
 Baka::Application.initialize!
