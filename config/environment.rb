e29edb3c1c9919da75473d4c531da61e2917f7b6
Davide Rizzi <sadjehwty@gmail.com>
2012-02-19 16:05:08 +0100

    eliminati messaggi di warning

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,4 @@ require File.expand_path('../application', __FILE__)
 #require 'mini_magick'
 # Initialize the rails application
 Mime::Type.register 'image/jpeg', :jpg
-Mime::Type.register 'image/gif',  :gif
-Mime::Type.register 'image/png',  :png
 Baka::Application.initialize!
