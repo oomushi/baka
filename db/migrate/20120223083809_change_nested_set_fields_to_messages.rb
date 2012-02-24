7e4ebc36ff084194dd72e87f2f0fb1585a812747
Davide Rizzi <sadjehwty@gmail.com>
2012-02-24 14:23:33 +0100

    terminata migrazione

--- db/migrate/20120223083809_change_nested_set_fields_to_messages.rb
+++ db/migrate/20120223083809_change_nested_set_fields_to_messages.rb
@@ -6,18 +6,21 @@ class ChangeNestedSetFieldsToMessages < ActiveRecord::Migration
     add_column :messages, :sdv, :integer,:default=>0,:limit=>8
     remove_column :messages, :rgt
     remove_column :messages, :lft
-    root=Message.find 1
-    root.nv,root.dv,root.snv,root.sdv=0,1,1,1
-    root.save
     begin
-      Message.joins(:message).where('messages_messages.dv>0 and messages.dv=0').readonly(false).each do |m| # forse si riesce a fare meglio non usando le joins
-        c=m.message.messages.where('dv>0').count+1
-        c-=1 if m.message.id==1
-        m.nv,m.dv,m.snv,m.sdv=m.message.nv+c*m.message.snv,m.message.dv+c*m.message.sdv,m.message.nv+(c+1)*m.message.snv,m.message.dv+(c+1)*m.message.sdv
-        m.save
-      end
-      tot=Message.joins(:message).where('messages_messages.dv>? and messages.dv=?',0,0).count
-    end while tot>0
+      root=Message.find 1
+      root.nv,root.dv,root.snv,root.sdv=0,1,1,1
+      root.save
+      begin
+        Message.joins(:message).where('messages_messages.dv>0 and messages.dv=0').readonly(false).each do |m| # forse si riesce a fare meglio non usando le joins
+          c=m.message.messages.where('dv>0').count+1
+          c-=1 if m.message.id==1
+          m.nv,m.dv,m.snv,m.sdv=m.message.nv+c*m.message.snv,m.message.dv+c*m.message.sdv,m.message.nv+(c+1)*m.message.snv,m.message.dv+(c+1)*m.message.sdv
+          m.save
+        end
+        tot=Message.joins(:message).where('messages_messages.dv>? and messages.dv=?',0,0).count
+      end while tot>0
+    rescue
+    end
   end
 
   def down
@@ -27,31 +30,35 @@ class ChangeNestedSetFieldsToMessages < ActiveRecord::Migration
     remove_column :messages, :dv
     remove_column :messages, :snv
     remove_column :messages, :sdv
-    stack=[Message.find(1)]
-    right=[]
-    i=1
-    while stack.length > 0
-      n=stack.shift
-      unless n.nil?
-        n.lft=i
-        i+=1
-        c= n.id==n.message_id ? 1 : 0
-        if n.messages.count>c 
-          stack.unshift nil
-          right.unshift n
-          stack.unshift(*n.messages)
-          stack.delete n
-        else
-          n.rgt=i
+    begin
+      n=Message.find(1)
+      stack=[n]
+      right=[]
+      i=1
+      while stack.length > 0
+        n=stack.shift
+        unless n.nil?
+          n.lft=i
           i+=1
+          c= n.id==n.message_id ? 1 : 0
+          if n.messages.count>c 
+            stack.unshift nil
+            right.unshift n
+            stack.unshift(*n.messages)
+            stack.delete n
+          else
+            n.rgt=i
+            i+=1
+          end
+          n.save
+        else
+          m=right.shift
+          m.rgt=i
+          i+=1
+          m.save
         end
-        n.save
-      else
-        m=right.shift
-        m.rgt=i
-        i+=1
-        m.save
       end
+    rescue
     end
   end
 end
