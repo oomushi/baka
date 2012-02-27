abf732b758ad254de36f160c55ec5ba9a8125676
Davide Rizzi <sadjehwty@gmail.com>
2012-02-27 18:56:59 +0100

    migliorata migration

--- db/migrate/20120227172738_create_poll_options_users.rb
+++ db/migrate/20120227172738_create_poll_options_users.rb
@@ -1,10 +1,11 @@
 class CreatePollOptionsUsers < ActiveRecord::Migration
   def change
-    create_table :poll_options_users do |t|
+    create_table :poll_options_users,:id=>false do |t|
       t.integer :user_id,:null=>false
       t.integer :poll_option_id,:null=>false
       t.integer :poll_id,:null=>false
     end
+    add_index :poll_options_users,[:user_id,:poll_option_id],:unique=>true
     add_index :poll_options_users,[:user_id,:poll_id],:unique=>true
   end
 end
