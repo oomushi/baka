d4af6bbf1c869833ad4eabd8a2349264786db629
Davide Rizzi <sadjehwty@gmail.com>
2012-02-28 20:59:33 +0100

    inizio metodo validazione poll_options_users

--- app/models/poll_option.rb
+++ app/models/poll_option.rb
@@ -1,4 +1,11 @@
 class PollOption < ActiveRecord::Base
   belongs_to :poll
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users,:uniq => true,:before_add => :unique?
+  protected
+  def unique? user
+    if user.poll_options.where('poll_id=?',self.poll_id).count>0
+      errors.add :base,"this user already gives his option"
+      false
+    end
+  end
 end
