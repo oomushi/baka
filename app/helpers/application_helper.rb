8f46892d39c3a69067ff2cd7f95ea4f86a0976ee
Davide Rizzi <sadjehwty@gmail.com>
2012-03-01 21:41:02 +0100

    inizio estensione bbcode

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,24 @@
 module ApplicationHelper
   def bb text
     if ! text.nil? and text!=''
-      bb = RbbCode::Parser.new
-      bb.parse(text).html_safe
+      cc=CCParser.instance
+      logger.debug cc.inspect
+      bb = RbbCode::Parser.new(:html_maker=>cc)
+      bb.parse(text).html_safe+cc.methods.sort.inspect
+    end
+  end
+  private
+  class CCParser < RbbCode::HtmlMaker # Singleton
+    include Singleton
+    def self._load
+=begin
+      bb=BBCode.all
+=end
+      name="html_from_query_tag"
+      name=name.to_sym
+      send :define_method, name do |node|
+        "<span class=\"cit\">#{node.inspect}</span>"
+      end
     end
   end
 end
