3853261c3cba853907677843994328323dd9123a
Davide Rizzi <sadjehwty@gmail.com>
2012-03-08 19:13:52 +0100

    mailer nuovi messaggi

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,11 +1,12 @@
 class MessageMailer < ActionMailer::Base
+  default :from => "admin@baka.macrobug.uchi.invalid"
   def new_message_alert message
     users=[]
     message.ancestors.where('follow = ?',true).each do |m|
       users<<m.user.email
     end
     users.uniq.each do |email|
-      mail :to=>email, :subject=>'New replay'
+      mail :to=>email, :subject=>'New replay',:body=>{:message=>message}
     end
   end
 end
