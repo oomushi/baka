8992217d3a734954d8630355877793e23e43a826
Davide Rizzi <sadjehwty@gmail.com>
2012-03-08 20:27:33 +0100

    mail di message sono in html (T_T)

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,12 +1,14 @@
 class MessageMailer < ActionMailer::Base
-  default :from => "admin@baka.macrobug.uchi.invalid"
+  default :from => "admin@baka.macrobug.uchi.invalid", 
+          :content_type =>  "text/html"
   def new_message_alert message
+    @message=message
     users=[]
     message.ancestors.where('follow = ?',true).each do |m|
       users<<m.user.email
     end
     users.uniq.each do |email|
-      mail :to=>email, :subject=>'New replay',:body=>{:message=>message}
+      mail :to=>email, :subject=>'New replay'
     end
   end
 end
