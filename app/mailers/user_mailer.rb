670644cb126b3c651832d18e44880e8e507ba6f2
Davide Rizzi <sadjehwty@gmail.com>
2012-03-09 21:44:07 +0100

    utente passato al mail viene passato anche alla vista

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ActionMailer::Base
   default :from => "admin@baka.macrobug.uchi.invalid"
   def email_confirmation user
+    @user=user
     mail(:to => user.email, :subject => "Confirm your email account")
   end
 end
