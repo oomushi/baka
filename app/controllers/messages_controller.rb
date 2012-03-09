d4d1033a181bc4e6f59634d9a06597be941f0444
Davide Rizzi <sadjehwty@gmail.com>
2012-03-09 18:28:34 +0100

    corretto redirect destroy message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -87,10 +87,11 @@ class MessagesController < ApplicationController
   def destroy
     @message = Message.find(params[:id])
     same_user? @message.user
+    id=@message.message_id
     @message.destroy
 
     respond_to do |format|
-      format.html { redirect_to messages_url }
+      format.html { redirect_to :action=>'show',:id=>id }
       format.json { head :no_content }
     end
   end
