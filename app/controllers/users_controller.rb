b468eaf51a5fd96427ff3a8c3b90b4ef0950b3a3
Davide Rizzi <sadjehwty@gmail.com>
2012-03-12 18:42:27 +0100

    diviso il controllo captcha da quello utente

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,10 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
 
     respond_to do |format|
-      if @user.save and recaptcha_valid?
+      if !recaptcha_valid?
+        format.html { render action: "new", :alert=> "captcha doesn't match" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      elsif @user.save 
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
