52181e2d3c1a46a1bd3a1c3f6efee505cb625dd0
Davide Rizzi <sadjehwty@gmail.com>
2012-03-16 19:09:28 +0100

    eliminato metodo errato in poll

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,10 +2,6 @@ class Poll < ActiveRecord::Base
   belongs_to :message
   has_many :poll_options,:dependent=>:destroy
   accepts_nested_attributes_for :poll_options, :allow_destroy => true
-  def initialize a,b
-    super a,b
-    poll_options.build if poll_options.count.zero?
-  end
   def options_count
     poll_options.inject{ |memo, o| memo+=o.users.count }
   end
