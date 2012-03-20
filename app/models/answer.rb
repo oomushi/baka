b878fadb927d0a15431991114ba4b68e6043cd35
Davide Rizzi <sadjehwty@gmail.com>
2012-03-20 20:42:13 +0100

    aggiunto metodo in answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,8 @@ class Answer < ActiveRecord::Base
   belongs_to :poll
   has_many :answers_users,:dependent=>:destroy
   has_many :users,:through => :answers_users,:dependent=>:destroy
+  def vote user
+    self.users<<user
+    save
+  end
 end
