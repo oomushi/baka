545e5202735a01262d5beb3fd9c40ba0a99dc48e
Davide Rizzi <sadjehwty@gmail.com>
2012-03-21 20:10:44 +0100

    evitato errore nel cancellare una associazione habtm

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :poll
-  has_many :answers_users,:dependent=>:destroy
-  has_many :users,:through => :answers_users,:dependent=>:destroy
+  has_and_belongs_to_many :users
   def vote user
     self.users<<user
     save
