f2c6a719b841eeb869532e6ba26b521460430d07
Davide Rizzi <sadjehwty@gmail.com>
2012-03-24 18:22:56 +0100

    miglioramento aspetto show message

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -2,4 +2,13 @@ module LikesHelper
   def link_to_submit text, params={}
     link_to_function text, "$(this).closest('form').submit()", params
   end
+  def num_reduce n
+    t,i=n.to_s,0
+    while t.length > 3
+      t=(t.to_i/1000).to_s
+      i+=1
+    end
+    a=(['']<<%w(K M G T P)).flatten
+    t+a[i]
+  end
 end
