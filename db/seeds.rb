e97dcaad4981f9223d1c573edde92fa47879e56e
Davide Rizzi <sadjehwty@gmail.com>
2012-04-02 18:56:06 +0200

    aggiunti gruppi base in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,31 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+a=Group.create({
+  :name=>"Admins",
+  :nv=>0,
+  :dv=>1,
+  :snv=>1,
+  :sdv=>1
+})
+Group.create({
+  :name=>"Moderetors",
+  :nv=>1,
+  :dv=>2,
+  :snv=>2,
+  :sdv=>3,
+  :group_id=>a.id
+})
+b=Group.create({
+  :name=>"Users",
+  :nv=>1,
+  :dv=>1,
+  :snv=>2,
+  :sdv=>1
+})
 admin = User.create(:username=>'Admin', :email=>'admin@baka.com', :birthday=>Date.new(1982,12,29), :password=>'sunset')
 admin.confirm_code=nil
+admin.groups<<a
 admin.save
 avatar=admin.avatar
 avatar.url='/assets/users/admin.png'
@@ -23,3 +46,6 @@ root = Message.create({
   :snv=>1,
   :sdv=>1
 })
+root.reader=b
+root.writer=a
+root.save
