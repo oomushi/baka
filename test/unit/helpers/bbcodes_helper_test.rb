c33332ba9bf926fc9914da4f4eace2900dce4945
sadjehwty <sadjehwty@gmail.com>
2012-10-02 20:39:11 +0200

    primo tentativo test

--- test/unit/helpers/bbcodes_helper_test.rb
+++ test/unit/helpers/bbcodes_helper_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class BbcodesHelperTest < ActionView::TestCase
+  setup do
+    @s = "[quote='abc'][:P][quote=\"xyz\"][:B][spoiler][b]gdhsagdjsa[/b][/spoiler][quote=qwerty][/quote][/quote][/quote]"
+  end
+  test "should parse correct test" do
+    ret = bb @s
+    assert_equals "<fieldset><legend>abc</legend><blockquote><img src=\"/assets/p.png\"/><fieldset><legend>xyz</legend><blockquote><img src=\"/assets/b.png\"/><span class=\"spoiler\"><strong>gdhsagdjsa</strong></span></blockquote></fieldset></blockquote></fieldset>"
+  end
 end
