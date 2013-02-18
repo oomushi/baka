0975694a0e495ce98cb8bea0e30cd8f7f14d7c3a
sadjehwty <sadjehwty@gmail.com>
2013-02-18 19:26:40 +0100

    inizio uso contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,11 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :type,:value
+  
+  def to_s
+    case type
+      when 'email' then mail_to value,"&thinsp;".html_safe, { :encode=>:javascript, :title=> value,:class=>"button email"}
+      when 'website' then link_to value, "&thinsp;".html_safe, {class: 'website button', target: '_blank', title: t(:website).downcase}
+    end
+  end
 end
