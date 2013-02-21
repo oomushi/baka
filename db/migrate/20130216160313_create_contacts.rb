d3c67cdf0eb5493a6efbc1f027c5ee2dd9362f2e
sadjehwty <sadjehwty@gmail.com>
2013-02-21 19:09:42 +0100

    corrette migration

--- db/migrate/20130216160313_create_contacts.rb
+++ db/migrate/20130216160313_create_contacts.rb
@@ -1,5 +1,5 @@
 class CreateContacts < ActiveRecord::Migration
-  def up
+  def change
     create_table :contacts do |t|
       t.integer :user_id
       t.string :type
@@ -7,23 +7,5 @@ class CreateContacts < ActiveRecord::Migration
 
       t.timestamps
     end
-    User.all.each do |u|
-      Contact.create({user_id: u.id, type: 'email', value: u.email }) unless u.email.nil?
-      Contact.create({user_id: u.id, type: 'website', value: u.website }) unless u.website.nil?
-    end
-  end
-  
-  def down
-    Contact.all.each do |c|
-      case c.type
-        when 'email'
-          c.user.email=c.value
-          c.user.save
-        when 'website'
-          c.user.website=c.value
-          c.user.save
-      end
-    end
-    drop_table :contacts
   end
 end
--- db/migrate/20130218183609_rename_contacts_column_class.rb
+++ db/migrate/20130218183609_rename_contacts_column_class.rb
@@ -1,5 +1,22 @@
 class RenameContactsColumnClass < ActiveRecord::Migration
-  def change
+  def up
     rename_column :contacts, :class, :protocol
+    User.all.each do |u|
+      Contact.create({user_id: u.id, protocol: 'email', value: u.email }) unless u.email.nil?
+      Contact.create({user_id: u.id, protocol: 'website', value: u.website }) unless u.website.nil?
+    end
+  end
+  def down
+    Contact.all.each do |c|
+      case c.protocol
+        when 'email'
+          c.user.email=c.value
+          c.user.save
+        when 'website'
+          c.user.website=c.value
+          c.user.save
+      end
+    end
+    rename_column :contacts, :protocol, :class
   end
 end
