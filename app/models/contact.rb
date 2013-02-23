5fa5b9848c99740ba880401b343484d8f88503dd
sadjehwty <sadjehwty@gmail.com>
2013-02-23 18:35:57 +0100

    inizio validazione per i vari protocolli di contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,8 @@ class Contact < ActiveRecord::Base
   attr_accessible :value, :protocol, :user_id
   belongs_to :user
   validates_presence_of :protocol,:value
-  validates_format_of :website, :with => URI::regexp(%w(http https)), :allow_black=>true, :allow_nil=>true
+  validates_format_of :value, :with => URI::regexp(%w(http https)), :allow_black=>false, :allow_nil=>false, :if => Proc.new{|c| c.protocol.eql? 'website'}
+  validates_format_of :value, :with => /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/, :allow_black=>false, :allow_nil=>false, :if => Proc.new{|c| c.protocol.eql? 'email'} 
   
   ENUM=%w(website email)
 end
