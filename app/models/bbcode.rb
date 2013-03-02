6177e5bebc28e5074b382726ba26d3d4d596a4a6
sadjehwty <sadjehwty@gmail.com>
2013-03-02 14:16:39 +0100

    pulito bbcode

--- app/models/bbcode.rb
+++ app/models/bbcode.rb
@@ -17,51 +17,7 @@ class Bbcode < ActiveRecord::Base
     Bbcode.indexable_by? user
   end
   
-  def old_text _value,_inner
-    tmp=''
-    tmp+=self.value.gsub '?',_value unless self.value.eql? '' || self.value.nil?
-    tmp+=self.inner.gsub '?',_inner unless self.inner.eql? '' || self.inner.nil?
-    self.layout.gsub '?',tmp
-  end
-
-  def tag
-    $1 if self.output =~ /^\[:?(\w+)(=.+?)?\]/
-  end  
-
-  def may_contain_text
-    self.properties&1
-  end
-  def may_contain_text= value
-    self.properties=val ? self.properties|1 : self.properties&~1
-  end
-  def may_not_be_empty
-    self.properties&2
-  end
-  def may_not_be_empty= value
-    self.properties=val ? self.properties|2 : self.properties&~2
-  end
-  def may_not_be_nested
-    self.properties&4
-  end
-  def may_not_be_nested= value
-    self.properties=val ? self.properties|4 : self.properties&~4
-  end
-  def may_not_contain_text
-    self.properties&8
-  end
-  def may_not_contain_text= value
-    self.properties=val ? self.properties|8 : self.properties&~8
-  end
-  def must_be_empty
-    self.properties&16
-  end
-  def must_be_empty= value
-    self.properties=val ? self.properties|16 : self.properties&~16
-  end
-  def may_be_nested
-    self.properties&32
-  end
-  def may_be_nested= value
-    self.properties=val ? self.properties|32 : self.properties&~32
+  def example meta, content
+    output.sub('?',meta).sub('?',content)
   end
 end
