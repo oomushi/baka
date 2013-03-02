963ca96756053d6e500f596ae3ee6b4a0ca84a39
sadjehwty <sadjehwty@gmail.com>
2013-03-02 18:22:47 +0100

    terminata modifica all'helper di bbcode

--- app/helpers/bbcodes_helper.rb
+++ app/helpers/bbcodes_helper.rb
@@ -1,27 +1,29 @@
 module BbcodesHelper
   def bb text
     if ! text.nil? and text!=''
-      bb=MyBBCoder.instance
-      bb.text=Sanitize.clean(text.to_s)
+      bb=MyBBCoder.new Sanitize.clean(text.to_s)
       bb.to_html.gsub(/\n|\r\n/, "<br />").html_safe
     end
   end
   private
-  class MyBBCoder < BBCoder # Singleton
-    include Singleton
-    alias :text= :initialize
-    def initialize
-#     BBCoder.configuration.clear
-      BBCoder.configure do |bb|
-        Bbcode.all.each do |b|
-          if !b.tag.nil? && b.tag.length>0
-            bb.tag b.tag.to_sym, :singular => b.singular, :match=>Regexp.new(b.match) do
-              b.text.gsub('#{content}',content).gsub('#{meta}',meta)
+  class MyBBCoder < BBCoder
+    def initialize text
+      if @@unconfig
+#       BBCoder.configuration.clear
+        BBCoder.configure do |bb|
+          Bbcode.all.each do |b|
+            if !b.tag.nil? && b.tag.length>0
+              bb.tag b.tag.to_sym, :singular => b.singular, :match=>Regexp.new(b.match) do
+                b.text.gsub('#{content}',content).gsub('#{meta}',meta)
+              end
             end
           end
         end
+        @@unconfig=false
       end
+      super text
     end
-    public :text=
+    private
+    @@unconfig=true
   end
 end
