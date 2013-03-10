4b9e614e616f17ce706e58b804ba61688bddb74b
sadjehwty <sadjehwty@gmail.com>
2013-03-10 12:03:07 +0100

    migliorato aspetto menù articoli

--- app/assets/javascripts/attachments.js
+++ app/assets/javascripts/attachments.js
@@ -1,14 +1,28 @@
+/*
 $(function() {
   var dropArea = document.getElementById("dropbox");
   var dragover = function(evt) {
     evt.stopPropagation();
     evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
   };
   var drop = function(evt) {
     evt.stopPropagation();
     evt.preventDefault();
-    alert('qui');
+    var dt=evt.dataTransfer;
+    var files=dt.files;
+    for(var i=0;i<files.length;i++){
+      var fr=new FileReader();
+      fr.onload=function(evt){
+        var i=$('<input>').attr('type','hidden')
+                          .attr('name','pippo')
+                          .val(evt.target.result);
+        $('#dropbox').after(i);
+      };
+      fr.readAsDataURL(files[i]);
+    }
   };
   dropArea.addEventListener('dragover', dragover , false);
   dropArea.addEventListener('drop', drop , false);
-});
+  new MultiSelector($('#dropbox').first()).addElement($('#file_list').first());
+});*/
