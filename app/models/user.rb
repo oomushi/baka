fd8ae3027fdb79ece7bac9e6c1aef58680b014d0
sadjehwty <sadjehwty@gmail.com>
2013-04-03 19:09:16 +0200

    migliorato metodo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -92,6 +92,7 @@ class User < ActiveRecord::Base
   end
   protected
   def confirm_email
+    return unless self.confirm_code.nil?
     salt=BCrypt::Engine.generate_salt
     self.confirm_code=BCrypt::Engine.hash_secret("#{self.id} #{self.username} #{self.email} #{self.password_hash} #{self.password_salt} #{self.created_at.to_s} #{rand(29**29)}", salt)
     self.save
