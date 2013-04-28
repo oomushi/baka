cb7eee3be644c5686f09bd34b2300480d8559604
sadjehwty <sadjehwty@gmail.com>
2013-04-28 19:39:26 +0200

    prima versione attachments pseudofunzionante

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,9 @@
 module AttachmentsHelper
+  def new_attachment(name, f, association, options={})
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    id="new_#{association}"
+    f.fields_for(association, new_object, :child_index => id) do |builder|
+      render( :partial => association.to_s.pluralize + "/form", :locals=>{:f => builder})
+    end
+  end
 end
