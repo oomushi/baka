c714a46f458b8113e023da8586335b0f587d3835
Davide Rizzi <sadjehwty@gmail.com>
2013-12-06 19:18:21 +0100

    migrazione errata

--- db/migrate/20130302120023_remove_fields_to_bbcodes.rb
+++ db/migrate/20130302120023_remove_fields_to_bbcodes.rb
@@ -3,11 +3,11 @@ class RemoveFieldsToBbcodes < ActiveRecord::Migration
     remove_column :bbcodes, :layout
     remove_column :bbcodes, :value
     remove_column :bbcodes, :inner
-    remove_column :bbcodes, :properties
+    remove_column :bbcodes, :attributes
   end
 
   def down
-    add_column :bbcodes, :properties, :integer
+    add_column :bbcodes, :attributes, :integer
     add_column :bbcodes, :inner, :string
     add_column :bbcodes, :value, :string
     add_column :bbcodes, :layout, :string
