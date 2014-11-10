036e42b3421532446ca95af2f424e26d9ca4b3c6
Davide Rizzi <sadjehwty@gmail.com>
2014-11-10 23:02:06 +0100

    password guest random

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ admin.save
 avatar=admin.avatar
 avatar.url='/assets/admin.png'
 avatar.save
-guest=User.new(:username=>"guest",:password=>"sunset",:guest=>true)
+guest=User.new(:username=>"guest",:password=>(0...32).map{(' '..'~').to_a[rand(95)]}.join,:guest=>true)
 guest.contacts<<Contact.create(:value=>'guest@baka.com', :protocol=>'email')
 guest.groups=[c]
 guest.save
