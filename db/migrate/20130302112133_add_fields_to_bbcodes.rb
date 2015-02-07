40e9fbda1127cb0a6fca856c67e300a08eac1836
Davide Rizzi <sadjehwty@gmail.com>
2015-02-07 14:39:42 +0100

    corretto migrate per nuove installazioni

--- db/migrate/20130302112133_add_fields_to_bbcodes.rb
+++ db/migrate/20130302112133_add_fields_to_bbcodes.rb
@@ -4,21 +4,6 @@ class AddFieldsToBbcodes < ActiveRecord::Migration
     add_column :bbcodes, :text, :string, :null=>false, :default=>''
     add_column :bbcodes, :singular, :boolean, :null=>false,:default=>true
     add_column :bbcodes, :match, :string, :null=>false, :default=>'.+'
-    Bbcode.all.each do |b|
-      if b.output =~ /^\[(:.)/
-        b.tag=''
-        b.output=$1
-        b.label=$1
-      else
-        b.tag=b.label
-        value=b.value.gsub('?','#{meta}')
-        value<<b.inner.gsub('?','#{content}')
-        b.text=b.layout.gsub('?',value)
-      end
-      b.match='.+'
-      b.singular=!(b.properties&16).zero?
-      b.save
-    end
   end
   def down
     remove_column :bbcodes, :tag
