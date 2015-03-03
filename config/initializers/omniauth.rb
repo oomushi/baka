85abb969922c70c80f270c468755172a05b9b421
Davide Rizzi <sadjehwty@gmail.com>
2015-03-03 21:47:24 +0100

    aggiornate chiavi

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :yahoo, "dj0yJmk9MXpXTVdJQ3RJS09GJmQ9WVdrOWFEWlBlbmxoTXpBbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD03NA--", "a9fe09bc5b43642d9d9e425a57947025d820bbc4"
   provider :facebook, '449175338566765', 'fc5cf452a26d931528b4394ef3c7e953',
            scope: 'email,user_birthday,read_stream', display: 'popup'
-  provider :google_oauth2, '1094586320801-nvg6i4lnlikepj2tl5unqomc1qtm6m5i.apps.googleusercontent.com', 'U4M5H3HiQl1Fr7O8_024_Idm',
+  provider :google_oauth2, '927139304986-e0jefhtuhejh57e6j0v2ct50l4dod52h.apps.googleusercontent.com', '9bKrV4dXvgWmLhUGNCArEpo5',
            {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}},provider_ignores_state: true }
 end
