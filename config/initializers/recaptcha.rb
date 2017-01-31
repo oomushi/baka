4ec1c12e34d1890a00163299a31a3683bec5f2f8
Davide Rizzi <sadjehwty@gmail.com>
2017-01-31 20:44:59 +0100

    aggiornata configurazioni

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LcVgQITAAAAAHrUJApkYH1u27fBkNSxzG55EmTI'
-  config.private_key = '6LcVgQITAAAAAJYTODx3Z5KOuY348VgN1dVYkeyj'
+  config.site_key  = '6LcVgQITAAAAAHrUJApkYH1u27fBkNSxzG55EmTI'
+  config.secret_key = '6LcVgQITAAAAAJYTODx3Z5KOuY348VgN1dVYkeyj'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
   # Uncomment if you want to use the newer version of the API,
