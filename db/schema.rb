21678ad09db34f632a526d5b6393c9494a610043
Davide Rizzi <sadjehwty@gmail.com>
2019-07-20 18:57:45 +0200

    add gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_073423) do
+ActiveRecord::Schema.define(version: 2019_07_20_152508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 2019_07_20_073423) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "choices"
